f90ed47ee713063c8fcf0a9b4eac6eeb0678f194
Ben Fashan <benfashan71@msn.com>
2024-02-20 14:28:53 +0000

    ADD: datetimefield to post model

--- blog/models.py
+++ blog/models.py
@@ -11,5 +11,7 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField()
+    excerpt = models.TextField(blank=True)  
+    update_on = models.DateTimeField(auto_now=True)      
+           
 
