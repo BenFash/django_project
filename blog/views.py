55cf60f46f6c35e28803bc52011d98d3ae0c33f7
Ben Fashan <benfashan71@msn.com>
2024-02-22 18:18:21 +0000

    FIX: follow links

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,15 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
+# class based view
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
 
 
+
+## function based view
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
@@ -24,10 +27,11 @@ def post_detail(request, slug):
     """
 
     queryset = Post.objects.filter(status=1)
+    # this is a shortcut to get data or raise a https404error # same as render being a shortcut to load data to a templ and return it. 
     post = get_object_or_404(queryset, slug=slug)
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post, "coder": "Ben Fashan"},
     )
